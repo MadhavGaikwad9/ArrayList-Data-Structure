cfca87be98e1854489046d36e57457d15fddeb3b
MadhavGaikwad9 <madhavgaikwad98476@gmail.com>
2026-03-21 21:56:13 +0530

    ArrayList

---
+++ OperationsinArrayList.java
@@ -0,0 +1,24 @@
+//operation on ArrayList
+
+import java.util.ArrayList;
+
+public class OperationsinArrayList{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        //Add operation
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+        
+
+
+
+
+        }
+}
